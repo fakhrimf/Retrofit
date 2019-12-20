3a2262c37c21373310ae0ae4d5aa7894a2b6f150
fakhrimf <fakhrimf23@gmail.com>
2019-12-20 11:33:20 +0700

    added search feature to consumerapp

--- cinemalistfavorites/src/main/java/com/fakhrimf/cinemalistfavorites/favorite/FavoriteFragment.kt
+++ cinemalistfavorites/src/main/java/com/fakhrimf/cinemalistfavorites/favorite/FavoriteFragment.kt
@@ -1,15 +1,15 @@
 package com.fakhrimf.cinemalistfavorites.favorite
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.database.ContentObserver
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.Settings
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -25,6 +25,9 @@ import kotlinx.coroutines.*
 class FavoriteFragment : Fragment(), FavoriteUserActionListener {
     private lateinit var favoriteVM: FavoriteVM
     private lateinit var job: Job
+    private val adapter by lazy {
+        FavoriteListAdapter(favoriteVM.favoriteList, this@FavoriteFragment)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -34,7 +37,25 @@ class FavoriteFragment : Fragment(), FavoriteUserActionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-        favoriteVM = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(activity!!.application)).get(FavoriteVM::class.java)
+        favoriteVM =
+            ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(activity!!.application)).get(FavoriteVM::class.java)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.info -> {
+                Toast.makeText(context, getString(R.string.info_menu), Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.language -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                true
+            }
+            else -> {
+                true
+            }
+        }
     }
 
     @SuppressLint("Recycle")
@@ -46,11 +67,11 @@ class FavoriteFragment : Fragment(), FavoriteUserActionListener {
                 animate().alpha(TRANSPARENT_ALPHA).setDuration(DURATION).setListener(null)
             }
             job = GlobalScope.launch(Dispatchers.IO) {
-                val cursor = activity?.contentResolver?.query(CONTENT_URI,null,null,null,null)
+                val cursor = activity?.contentResolver?.query(CONTENT_URI, null, null, null, null)
                 favoriteVM.favoriteList = favoriteVM.cursorToArrayList(cursor!!)
                 delay(2000)
                 withContext(Dispatchers.Main) {
-                    rvFavorite?.adapter = FavoriteListAdapter(favoriteVM.favoriteList, this@FavoriteFragment)
+                    rvFavorite?.adapter = adapter
                     rvFavorite?.layoutManager = LinearLayoutManager(requireContext())
                     favoriteVM.setIsLoaded(true)
                     rvFavorite?.apply {
@@ -64,7 +85,8 @@ class FavoriteFragment : Fragment(), FavoriteUserActionListener {
             }
         } else {
             rvFavorite?.layoutManager = LinearLayoutManager(requireContext())
-            rvFavorite?.adapter = FavoriteListAdapter(favoriteVM.favoriteList, this@FavoriteFragment)
+            rvFavorite?.adapter =
+                FavoriteListAdapter(favoriteVM.favoriteList, this@FavoriteFragment)
             if (favoriteVM.favoriteList.isEmpty()) {
                 tvInfo.visibility = View.VISIBLE
             }
@@ -105,6 +127,46 @@ class FavoriteFragment : Fragment(), FavoriteUserActionListener {
         setRecycler()
         srl.setOnRefreshListener {
             refresh()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_favorite_consumer, menu)
+        val menuItem = menu.findItem(R.id.search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.queryHint = getString(R.string.search_query_hint_favorites)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                adapter.filter.filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                if (newText?.isEmpty() == true) adapter.resetList()
+                return false
+            }
+        })
+        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                setItemVisibility(menu, false)
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                setItemVisibility(menu, true)
+                return true
+            }
+        })
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    private fun setItemVisibility(menu: Menu, visible: Boolean) {
+        for (i in 0 until menu.size()) {
+            val item = menu.getItem(i)
+            if (item != menu.findItem(R.id.search)) item.isVisible = visible
+            activity?.invalidateOptionsMenu()
         }
     }
 
